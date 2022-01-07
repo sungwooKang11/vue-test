287a47b2bc8a587ae0296818e555037a9233b440
sungwookang <sungwookang11@gmail.com>
2022-01-07 12:06:20 +0900

    개별 버튼 만들기

--- apple_test/src/App.vue
+++ apple_test/src/App.vue
@@ -8,23 +8,42 @@
   <div>
     <h4>{{ products[0] }}</h4>
     <p>500만원</p>
+    <button @click="increase">허위매물신고</button> <span>신고수 :{{신고수}}</span>
   </div>
   <div>
     <h4>{{ products[1] }}</h4>
     <p>200만원</p>
+    <button @click="increase1">허위매물신고</button> <span>신고수 :{{신고수1}}</span>
+  </div>
+    <div>
+    <h4>{{ products[2] }}</h4>
+    <p>100만원</p>
+    <button @click="increase2">허위매물신고</button> <span>신고수 :{{신고수2}}</span>
   </div>
 
 </template>
 
 <script>
-
-
 export default {
   name: 'App',
   data(){
     return{
       menus : ['Home', 'Products', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      신고수 : '0',
+      신고수1 : '0',
+      신고수2 : '0',
+    }
+  },
+  methods : {
+    increase(){
+      this.신고수 ++ ;
+    },
+    increase1(){
+      this.신고수1 ++ ;
+    },
+    increase2(){
+      this.신고수2 ++ ;
     }
   },
   components: {
@@ -44,7 +63,7 @@ export default {
 
 .menu {
   background: darkslateblue;
-  padding: 15px;
+  padding: 20px;
   border-radius: 5px;
 }
 
